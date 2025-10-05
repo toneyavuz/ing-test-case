fc1b93ba1fd4c832a463c1dcef9fdca2685b4c43
toneyavuz <toneyavuz@gmail.com>
2025-10-05 13:26:06 +0300

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ing-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.scss'
+})
+export class App {
+  protected title = 'ing-test-case';
+}
